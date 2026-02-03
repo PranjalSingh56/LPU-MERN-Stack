f3b665ddb8db2a2aff309d4568da573b85d84a33
Pranjal500 <mrpranjal917@gmail.com>
2026-02-03 17:33:50 +0530

    Added more code

--- Day11/http/index.js
+++ Day11/http/index.js
@@ -1,7 +1,8 @@
 import http from "http";
-
+import fs from "fs";
+import path from "path";
 const host = 'localhost';
-const port = 3000;
+const port = 4000;
 
 // const server = http.createServer((req, res) => {
 //     // res.setHeader('Content-Type', 'text/plain');
@@ -17,7 +18,7 @@ const port = 3000;
 // });
 
 
-
+/*
 const server = http.createServer((req, res) => {
     const responseObj ={
     success:true,
@@ -34,4 +35,32 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
+});
+
+*/
+
+
+/*
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("index.html", "utf-8"));
+});
+server.listen(3001, () => {
+    console.log("SERVER running at http://localhost:3001");
+});
+*/
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(fs.readFileSync("index.html", "utf-8"));
+    }
+    else if (req.url === "/style.css") {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(fs.readFileSync("style.css", "utf-8"));
+    }
+});
+
+server.listen(3001, () => {
+    console.log("SERVER running at http://localhost:3001");
 });
